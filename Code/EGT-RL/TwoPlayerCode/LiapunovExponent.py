36f29f6d9cb7ef58d405114de30b9148e85c94aa
Aamal Hussain <ah3815@ic.ac.uk>
2020-08-12 20:25:56 +0100

    Generalised Code to p-players

--- Code/EGT-RL/TwoPlayerCode/LiapunovExponent.py
+++ Code/EGT-RL/TwoPlayerCode/LiapunovExponent.py
@@ -1,18 +1,24 @@
 import numpy as np
+import itertools
+import scipy.sparse as sps
 import matplotlib.pyplot as plt
 from time import time
 from tqdm import tqdm
 
+
 alpha = .1
 gamma = .1
 Gamma = 0.1
-tau = 1
+tau = 0.05
 
 delta_0 = 1e-3
 nSim = 5
-nIter = 1e5
+nIter = int(1e4)
 
-def generateGame(gamma, nAct):
+nPlayers = 3
+nActions = 2
+
+def generateGames(gamma, nSim, nAct, nPlayers):
     """
     Draw a random payoff matrix from a multivariate Gaussian, currently the unscaled version.
 
@@ -22,69 +28,81 @@ def generateGame(gamma, nAct):
     [reward1s, reward2s]: list of payoff matrices
     """
 
-    nElements = nAct ** 2  # number of payoff elements in the matrix
+    nElements = nAct ** nPlayers  # number of payoff elements in a player's matrix
 
-    cov = np.eye(2 * nElements)  # <a_ij^2> = <b_ji^2> = 1
-    cov[:nElements, nElements:] = np.eye(nElements) * gamma  # <a_ij b_ji> = Gamma
-    cov[nElements:, :nElements] = np.eye(nElements) * gamma
+    cov = sps.lil_matrix(sps.eye(nPlayers * nElements))
+    idX = list(itertools.product(list(range(1, nAct + 1)), repeat=nPlayers)) * nPlayers
+    for idx in range(nElements):
+        for b in range(1, nPlayers):
+            rollPlayers = np.where([np.all(idX[i] == np.roll(idX[idx], -1 * b)) for i in range(len(idX))])[0][b]
+            cov[idx, rollPlayers], cov[rollPlayers, idx] = gamma/(nPlayers - 1), gamma/(nPlayers - 1)
 
-    rewards = np.random.multivariate_normal(np.zeros(2 * nElements), cov=cov)
+    cov = cov.toarray()
 
-    reward1s = rewards[0:nElements].reshape((nAct, nAct))
-    reward2s = rewards[nElements:].reshape((nAct, nAct)).T
+    allPayoffs = np.zeros(shape=(nPlayers, nElements))
 
-    return [reward1s, reward2s]
+    for i in range(nSim):
+        rewards = np.random.multivariate_normal(np.zeros(nPlayers * nElements), cov=cov)
+        each = np.array_split(rewards, nPlayers)
+        allPayoffs = np.dstack((allPayoffs, np.array(each)))
+
+    return allPayoffs
+
 
 def getActionProbs(qValues):
-    partitionFunction = [np.sum([np.exp(tau * i) for i in qValues[p, :]]) for p in range(2)]
-    actionProbs = np.array([[np.exp(tau * i) / partitionFunction[p] for i in qValues[p, :]] for p in range(2)])
-    
+    partitionFunction = np.expand_dims(np.sum(np.exp(tau * qValues), axis=1), axis = 1)
+    partitionFunction = np.hstack([partitionFunction]*nActions)
+    actionProbs = np.exp(tau * qValues)/partitionFunction
     return actionProbs
 
-def qUpdate(qValues):
+
+def qUpdate(qValues, payoffs):
+    idX = list(itertools.product(list(range(0, nActions)), repeat=nPlayers))
     actionProbs = getActionProbs(qValues)
+    bChoice = np.array([[np.random.choice([0, 1], p=actionProbs[p, :, s]) for s in range(nSim)] for p in range(nPlayers)])
 
-    boltzmannChoices = [np.random.choice([0, 1], p=actionProbs[p, :]) for p in range(2)]
-
-    rewards = [payoffA[boltzmannChoices[0], boltzmannChoices[1]],
-                       payoffB[boltzmannChoices[0], boltzmannChoices[1]]]
-
-    qValues[0, boltzmannChoices[0]] += alpha * (rewards[0] - qValues[0, boltzmannChoices[0]] + gamma * max(qValues[0, :]))
-    
-    qValues[1, boltzmannChoices[1]] += alpha * (rewards[1] - qValues[1, boltzmannChoices[1]] + gamma * max(qValues[1, :]))
+    rewards = np.array([[payoffs[p, np.where([np.all(idX[i] == np.roll(bChoice[:, s], -1 * p)) for i in range(len(idX))])[0][0], s] for s in range(nSim)] for p in range(nPlayers)])
+    for s in range(nSim):
+        qValues[range(nPlayers), bChoice[:, s], s] += alpha * (rewards[:, s] - qValues[range(3), bChoice[:, s], s] + gamma * np.max(qValues[:, :, s], axis = 1))
 
     return qValues
 
-all_actionProbs = np.eye(2)
 
-allExpo = np.zeros((10, 10))
+def getDelta(qValues0, qValues1):
+    actionProbs0, actionProbs1 = getActionProbs(qValues0), getActionProbs(qValues1)
+    return np.mean(abs(actionProbs1 - actionProbs0), axis=2)
 
+
+# allExpo = np.zeros((10, 10, 10))
+plotExpo = []
 i = 0
 for alpha in tqdm(np.linspace(0, 1, num=10)):
     j = 1
-    for Gamma in np.linspace(-1, 1, num=10):
+    for tau in np.linspace(1, 10, num=10):
+        k = 0
+        for Gamma in np.linspace(-1, 1, num=10):
 
-        for cSim in range(nSim):
+            all_actionProbs = np.eye(2)
 
-            payoffA, payoffB = generateGame(Gamma, 2)
+            payoffs = generateGames(Gamma, nSim, nActions, nPlayers)
 
-            qValues0 = np.random.rand(2, 2)
-            qValues1 = qValues0 + np.array([np.random.choice([-delta_0, delta_0]) for i in range(4)]).reshape((2, 2))
+            qValues0 = np.random.rand(nPlayers, nActions, nSim)
+            qValues1 = np.random.rand(nPlayers, nActions, nSim)
 
-            for cIter in range(int(nIter)):
-                start = time()
-                qValues0, qValues1 = qUpdate(qValues0), qUpdate(qValues1)
-                print(time() - start)
-            actionProbs0, actionProbs1 = getActionProbs(qValues0), getActionProbs(qValues1)
+            for cIter in range(nIter):
+                # for i in range(nSim):
+                #     all_actionProbs = np.dstack((all_actionProbs, (getActionProbs(qValues0, nSim)[i, :, :])))
+                if cIter % 300 == 0 and cIter != 0:
+                    delta_1 = getDelta(qValues0, qValues1)
+                qValues0, qValues1 = qUpdate(qValues0, payoffs), qUpdate(qValues1, payoffs)
 
-            all_actionProbs = np.dstack((all_actionProbs, abs(actionProbs1 - actionProbs0)))
+            delta_n = getDelta(qValues0, qValues1)
 
-        actionDiff = np.sum(all_actionProbs[:, :, 1:], axis=2)/nSim
-        delta_n = np.reshape(actionDiff, (1, 4))
+            liapExp = np.max((1 / nIter) * np.log(delta_n / delta_1))
 
-        liapExp = np.max((1/nIter) * np.log(delta_n/delta_0))
+            plotExpo += [np.array([alpha, tau, Gamma, liapExp])]
 
-        allExpo[10 - j, i] = liapExp
-
+            # allExpo[10 - j, i, k] = liapExp
+            k += 1
         j += 1
     i += 1
